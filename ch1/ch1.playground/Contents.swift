823536bf4180ffbe488a4bedb446c6c518c8b3f7
johnl <johnl@wealthfront.com>
2016-04-11 16:53:06 -0700

    inital

---
+++ ch1/ch1.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// 1.1
+// Implement an algorithm to determin if a string has all unique characters. What if you cannot use additional data structures?
+func isUniqueString(input : String) {
+  
+}
